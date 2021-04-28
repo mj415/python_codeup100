46b7dd8fec8850f18bff1e456f7c506ad364ebf6
mj415 <70506528+mj415@users.noreply.github.com>
2021-04-28 22:21:17 +0900

    Add files via upload

---
+++ 6021.py
@@ -0,0 +1,6 @@
+# 6021 : 단어 1개 입력받아 나누어 출력하기
+
+s = input()  # 5 글자 입력
+
+for i in range(len(s)):
+    print(s[i])
---
+++ 6025.py
@@ -0,0 +1,7 @@
+# 6025 : 정수 2개 입력받아 합 계산하기
+
+a, b = input().split()  # int(input().split())는 int()가 list가 아니라 오류
+c = int(a)+int(b)
+
+print(int(a)+int(b))
+print(c)
