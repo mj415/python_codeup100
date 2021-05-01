0666e70a313cef41402d4b8b4a3ccdd04c898aec
mj415 <70506528+mj415@users.noreply.github.com>
2021-05-01 10:38:49 +0900

    Add files via upload

---
+++ 6035.py
@@ -0,0 +1,6 @@
+# 6035 : 실수 2개 입력받아 곱 계산하기
+
+a, b = input().split()  # 공백을 두고 입력받은 값을 a와 b에 할당
+
+f = float(a)*float(b)
+print(f)
---
+++ 6039.py
@@ -0,0 +1,6 @@
+# 6039 : 실수 2개 입력받아 거듭제곱 계산하기
+
+a, b = input().split()
+c = float(a)**float(b)
+
+print(c)
